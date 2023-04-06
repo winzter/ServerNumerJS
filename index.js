1556d0a2c2dc46cad19227a8656ce47977ed37c2
Phakin Neamson <92076305+winzter@users.noreply.github.com>
2023-04-06 16:03:07 +0700

    test

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const mongoose = require('mongoose')
 
 mongoose.connect('mongodb+srv://admin:1234@cluster0.cfglwqy.mongodb.net/?retryWrites=true&w=majority')
     .then(()=> console.log('connection success!'))
     .catch((err)=>console.error(err))
 
+const corsOption = {
+    origin:"http://localhost:3000",
+    credentials:true
+}
+
+const data = [
+    {
+        "value":`{
+            "index":"0",
+            "label": "(x^4)-13",
+            "xl":"0",
+            "xr":"2"
+        }`,
+        "label": '(x^4)-13',
+    },
+    {
+        "value":`{
+            "index":"1",
+            "label": "(x^2)-7",
+            "xl":"1",
+            "xr":"2"
+        }`, 
+        "label": '(x^2)-16',
+    }
+]
+
+app.use(cors(corsOption))
+
 app.get('/',(req,res)=>{
-    res.send("HI")
+    res.send(data)
 })
 
 app.listen(5000,()=>{
