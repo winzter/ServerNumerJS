61f4c4bc7b889234ef5337614cf8cccd0c9a5c8f
Phakin Neamson <92076305+winzter@users.noreply.github.com>
2023-04-08 20:05:05 +0700

    swagger

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const SwaggerUI = require("swagger-ui-express")
 const mongoose = require('mongoose')
 const MathModel = require('./model/MathModel')
 const bodyparser = require('body-parser')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 const {MONGO_USERNAME,MONGO_PASSWORD} = process.env;
+
 
 mongoose.connect(`mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.cfglwqy.mongodb.net/?retryWrites=true&w=majority`)
 .then(()=> console.log('connection success!'))
@@ -23,23 +25,59 @@ app.use(bodyparser.urlencoded({ extended: false }))
 app.use(express.json());
 app.use(cors(corsOption))
 
+const secretKey = "Haeykcoromus"
+let token = jwt.sign({name:"Win"},secretKey)
+
+const verifyToken = (req,res,next)=>{
+    let auth = req.headers.authorization
+    if(!token){
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
+    try{
+        auth = auth.split(" ")[1]
+        let decode = jwt.verify(auth,secretKey)
+        req.name = decode
+        next()
+    }catch(err){
+        res.send("Wrong Auth")
+        console.log(err);
+    }
+    
+}
+
+app.get("/gentoken/:name",(req,res)=>{
+    let { name } = req.params
+    //console.log(name);
+    token = jwt.sign({name},secretKey)
+    res.send(token)
+})
+
+app.get("/checktoken",verifyToken,(req,res)=>{
+    let {name} = req.name 
+    res.send(`You are ${name}`)
+})
+
+
+
 app.get("/api/rootofequation/:topic",async(req,res)=>{
     let { topic } = req.params
     let find = await MathModel.find({topic:`${topic}`})
-        res.json(find)
+    res.json(find)
     
 })
 
 
-app.post("/api/postdata",async(req,res)=>{
-    let {value,equation,xl,xr,label,group} = req.body
-    await MathModel.create({value,equation,xl,xr,label,group})
+app.post("/api/postdata",(req,res)=>{
+    console.log(req.body);
+    let {value,equation,xl,xr,label,group,topic} = req.body
+    MathModel.create({value,equation,xl,xr,label,group,topic})
     res.send({ status: 'SUCCESS' })
 })
 
     
 
 app.use("/docs",SwaggerUI.serve,SwaggerUI.setup(Swagger))
+
 app.listen(5000,()=>{
     console.log('Listening on port 5000');
 })
